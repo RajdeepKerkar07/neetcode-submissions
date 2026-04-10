70e814e32ae4ad9fd47b05a6f9173b882164a77e
Rajdeep Kerkar <43118870+RajdeepKerkar07@users.noreply.github.com>
2026-04-10 14:18:57 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        string s_new = new string(s.OrderBy(c => c).ToArray());
+        string t_new = new string(t.OrderBy(c => c).ToArray());
+        Console.WriteLine(s_new);
+        Console.WriteLine(t_new);
+        if(s_new.Count() != t_new.Count()) return false;
+        for(int i=0; i < s_new.Count(); i++)
+        {
+            if(s_new[i] != t_new[i]) return false;
+        }
+        return true;
+    }
+}
