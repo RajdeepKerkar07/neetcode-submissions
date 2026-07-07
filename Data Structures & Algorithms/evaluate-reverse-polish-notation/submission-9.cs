4790bc5c2208b0a241396e80751a5ab61e1caffc
Rajdeep Kerkar <43118870+RajdeepKerkar07@users.noreply.github.com>
2026-07-08 01:42:34 +0530

    Add: evaluate-reverse-polish-notation - submission-9

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-9.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        Stack<int> expression = new Stack<int>();
+        string[] operations = ["+", "-", "*", "/"];
+        foreach(var c in tokens){
+            if(operations.Contains(c)){
+                int result = 0;
+                var temp2 = expression.Pop();
+                var temp1 = expression.Pop();
+                switch(c){
+                    case "+":
+                        result = temp1 + temp2;
+                        break;
+                    case "-":
+                        result = temp1 - temp2;
+                        break;
+                    case "*":
+                        result = temp1 * temp2;
+                        break;
+                    case "/":
+                        result = temp1 / temp2;
+                        break;
+                }
+                expression.Push(result);
+            }
+            else{
+                expression.Push(Convert.ToInt32(c));
+            }
+        }
+        return expression.Peek();
+    }
+}
